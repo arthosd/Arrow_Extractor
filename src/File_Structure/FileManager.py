3cb53beb6e677d6df7423dcf4de033acc6f7befa
arthosd <elie.debs@outlook.fr>
2021-03-04 12:41:25 +0100

    Ajout de la fonction de categorisations

--- src/File_Structure/FileManager.py
+++ src/File_Structure/FileManager.py
@@ -1,24 +1,46 @@
 import os
 
-class File_Manager : 
 
-    def __init__(self,path_directory):
+class File_Manager:
+
+    def __init__(self, path_directory):
         self.files_in_directory = os.listdir(path_directory)
         self.path_directory = path_directory
         pass
 
-    def show_files (self):
+    def show_files(self):
         for file in self.files_in_directory:
             print(file)
         pass
 
-    def categorize (self):
+    def categorize(self):
 
-        array = dict()# Dictionaire contenant toute les clés
+        array = dict()  # Dictionaire contenant toute les clés
 
-        for file in self.files_in_directory:  
-             debut_nom_fichier = file[0:4]#On récupere le début de chaques nom
+        for file in self.files_in_directory:
+            # On récupere le début de chaques nom
+            debut_nom_fichier = file[0:4]
 
-             if array.has_key(debut_nom_fichier) == True :
-                 file_to_copy = file(self.path_directory+"/"+file,"rb")# Open in binary
-                 file_where_to_copy = file(self.path_directory+"/"+debut_nom_fichier+"/"+file,"wb")#Open in binary
+            if debut_nom_fichier in array:
+                file_to_copy = open(self.path_directory +
+                                    "/"+file, "rb")  # Open in binary
+                file_where_to_copy = open(
+                    self.path_directory+"/"+debut_nom_fichier+"/"+file, "wb")  # Open in binary
+                binary = file_to_copy.read()
+                file_where_to_copy.write(binary)
+                file_to_copy.close()
+                file_where_to_copy.close()
+
+            else:
+                # Si la clée n'existe pas alors on la créer
+                array[debut_nom_fichier] = True
+                # On crée l'entrée dans le dict
+                os.makedirs(self.path_directory+"/"+debut_nom_fichier)
+                file_to_copy = open(self.path_directory +
+                                    "/"+file, "rb")  # Open in binary
+                file_where_to_copy = open(
+                    self.path_directory+"/"+debut_nom_fichier+"/"+file, "wb")  # Open in binary
+                binary = file_to_copy.read()
+                file_where_to_copy.write(binary)
+                file_to_copy.close()
+                file_where_to_copy.close()
--- src/init.py
+++ src/init.py
@@ -1,7 +1,7 @@
 import os
 from File_Structure.FileManager import *
 
-manager = File_Manager("/home/elie/Documents/Projet/Fac/Arrow_Extractor/RESJPG")
-#manager.show_files()
+manager = File_Manager(
+    "/home/elie/Documents/Fac/Projet/Arrow_Extractor/RESJPG")
+# manager.show_files()
 manager.categorize()
-
