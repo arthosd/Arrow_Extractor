23012275bf012e147c69dfddc19ba28bce704e23
arthosd <elie.debs@outlook.fr>
2021-03-05 14:32:14 +0100

    Preparation au merge

--- src/init.py
+++ src/init.py
@@ -1,8 +1,1 @@
 import os
-from File_Structure.FileManager import *
-
-manager = File_Manager(
-    "/home/elie/Documents/Fac/Projet/Arrow_Extractor/RESJPG")
-
-for file in manager.directories:
-    print(file)
