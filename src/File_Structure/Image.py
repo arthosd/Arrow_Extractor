ab8bf473b78c2b17e462c615461103f26028a82a
arthosd <elie.debs@outlook.fr>
2021-03-06 15:07:36 +0100

    Il faut retirer les  petit composent

--- src/File_Structure/Image.py
+++ src/File_Structure/Image.py
@@ -2,20 +2,41 @@ import os
 import cv2
 import numpy as np
 import json
-
+import math
+from scipy.ndimage.measurements import label
 
 class Image :
     
-    def __init__(self,list_of_dir,path):
+    def __init__(self,image_path):
 
-        self.directories = list_of_dir# Liste des repertoires contenant les images
-        self.path_to_write = path# Il faut lui rajouter un dossier
-        self.data = {} #Dict where all the images data will be written
+        self.image_path = image_path
+        self.labels = None
+        self.num_labels = None
+        self.image = cv2.imread(image_path,0)
 
         pass
 
+    def remove_connected_comp(self,segmented_img, connected_comp_diam_limit=20):
+        """
+        Remove connected components of a binary image that are less than smaller than specified diameter.
+        :param segmented_img: Binary image.
+        :param connected_comp_diam_limit: Diameter limit
+        :return:
+        """
+        img = segmented_img.copy()
+        structure = np.ones((3, 3), dtype=np.int)
+        labeled, n_components = label(img, structure)
+        for i in range(n_components):
+            ixy = np.array(list(zip(*np.where(labeled == i))))
+            x1, y1 = ixy[0]
+            x2, y2 = ixy[-1]
+            dst = math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2)
+            if dst < connected_comp_diam_limit:
+                for u, v in ixy:
+                    img[u, v] = 0
+        return img
 
-    def _imshow_components(self,labels):
+    def imshow_components(self,labels):
         label_hue = np.uint8(179*labels/np.max(labels))
         blank_ch = 255*np.ones_like(label_hue)
         labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
@@ -27,6 +48,9 @@ class Image :
         cv2.imshow('labeled.png', labeled_img)
         cv2.waitKey()
 
-    def _connected_compomnent (self):
-        
+    def find_connected_component (self):
+
+        self.num_labels ,self.labels = cv2.connectedComponents(self.image)
+        print (self.labels.dtype)
+
         pass
--- src/init.py
+++ src/init.py
@@ -1,1 +1,5 @@
 import os
+from File_Structure.Image import Image
+
+image = Image ('/home/elie/Documents/Projet/Fac/Arrow_Extractor/RESJPG/E110/E1106113.jpg.pgm.jpg')
+image.find_connected_component()
