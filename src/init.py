b7d673feb4a70176f9ad6ece511584981eec5173
arthosd <elie.debs@outlook.fr>
2021-03-12 19:17:44 +0100

    Restructurations du projet

--- src/init.py
+++ src/init.py
@@ -2,7 +2,7 @@ import os
 from File_Structure.Image import Image
 from File_Structure.FileManager import File_Manager
 
-dossier = File_Manager('/home/elie/Documents/Projet/Fac/Arrow_Extractor/RESJPG/E110')
+"""dossier = File_Manager('/home/elie/Documents/Projet/Fac/Arrow_Extractor/RESJPG/E110')
 
 images = dossier.files_in_directory #On récupère les images
 
@@ -12,4 +12,6 @@ for item in images :
     image = Image(dossier.path_directory,item) # On crée une image
     image.get_connected_component()# On récupère les connected component
     image.write_components(1000, compteur)
-    compteur = compteur +1
+    compteur = compteur +1"""
+
+
