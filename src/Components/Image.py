1d21a214135795a52ac104022249a3a04dba2db1
arthosd <elie.debs@outlook.fr>
2021-03-13 14:52:56 +0100

    Ajout de la fonction permettant d'écrire une image centrée sur le composent.

--- src/Components/Image.py
+++ src/Components/Image.py
@@ -5,13 +5,13 @@ import numpy as np
 
 
 class Image:
-    
+
     def __init__(self, directory_path, image_name):
 
         self.image_path = directory_path+"/"+image_name
         self.directory_path = directory_path
         self.image_name = image_name
-        self.image = cv2.imread(directory_path+"/"+image_name,0)
+        self.image = cv2.imread(directory_path+"/"+image_name, 0)
         self.stats = None
         self.nb_component = None
 
@@ -20,32 +20,63 @@ class Image:
         """
         Récupère les composantes connexe à partir d'une de l'image chargé
         """
-    def get_connected_component (self):
 
-        self.nb_component , labels, self.stats, centroid = cv2.connectedComponentsWithStats(self.image,connectivity=4)
+    def get_connected_component(self):
+
+        self.nb_component, labels, self.stats, centroid = cv2.connectedComponentsWithStats(
+            self.image, connectivity=4)
 
         pass
 
-    def write_components (self,seuil,index):
+    def write_components(self, seuil_inferieur, index):
+
+        compteur = 0
+
+        for i in range(0, self.nb_component):
+            if self.stats[i, cv2.CC_STAT_AREA] > seuil_inferieur:  # Si on est au dessus du seuil
+                debutX = self.stats[i, cv2.CC_STAT_LEFT]
+                debutY = self.stats[i, cv2.CC_STAT_TOP]
+                horizontal = self.stats[i, cv2.CC_STAT_WIDTH]
+                vertical = self.stats[i, cv2.CC_STAT_HEIGHT]
+                newImage = np.zeros((horizontal+1, vertical+1))
+
+                for x in range(debutX, debutX+horizontal):
+                    for y in range(debutY, debutY+vertical):
+                        newImage[x-debutX, y-debutY] = self.image[y, x]
+
+                cv2.imwrite(self.directory_path+"/"+"connex" +
+                            str(i*index/2)+'.jpg', newImage)
+
+            else:
+                compteur = compteur + 1
+
+        return compteur
+
+    """
+    Fonction à ré écrire
+    """
+
+    def reconstruct_image(self, seuil, index):
 
         compteur = 0
         sizeX, sizeY = self.image.shape
 
-        for i in range (0, self.nb_component):
-            if self.stats[i,cv2.CC_STAT_AREA] > seuil : #Si on est au dessus du seuil
-                newImage = np.zeros((sizeX,sizeY))
-                debutX = self.stats[i,cv2.CC_STAT_LEFT]
-                debutY = self.stats[i,cv2.CC_STAT_TOP]
-                horizontal = self.stats[i,cv2.CC_STAT_WIDTH]
-                vertical = self.stats[i,cv2.CC_STAT_HEIGHT]
-                
+        for i in range(0, self.nb_component):
+            if self.stats[i, cv2.CC_STAT_AREA] > seuil:  # Si on est au dessus du seuil
+                newImage = np.zeros((sizeX, sizeY))
+                debutX = self.stats[i, cv2.CC_STAT_LEFT]
+                debutY = self.stats[i, cv2.CC_STAT_TOP]
+                horizontal = self.stats[i, cv2.CC_STAT_WIDTH]
+                vertical = self.stats[i, cv2.CC_STAT_HEIGHT]
+
                 for x in range(debutX, debutX+horizontal):
                     for y in range(debutY, debutY+vertical):
-                        newImage [y,x] = self.image[y,x]
+                        newImage[y, x] = self.image[y, x]
 
-                cv2.imwrite(self.directory_path+"/"+"connex"+str(i*index/2)+'.jpg',newImage)
+                cv2.imwrite(self.directory_path+"/"+"connex" +
+                            str(i*index/2)+'.jpg', newImage)
 
             else:
-                compteur = compteur +1
+                compteur = compteur + 1
 
         return compteur
--- src/init.py
+++ src/init.py
@@ -1,17 +1,3 @@
 import os
-from File_Structure.Image import Image
+from Components.Image import Image
 from File_Structure.FileManager import File_Manager
-
-"""dossier = File_Manager('/home/elie/Documents/Projet/Fac/Arrow_Extractor/RESJPG/E110')
-
-images = dossier.files_in_directory #On récupère les images
-
-compteur = 1
-
-for item in images :
-    image = Image(dossier.path_directory,item) # On crée une image
-    image.get_connected_component()# On récupère les connected component
-    image.write_components(1000, compteur)
-    compteur = compteur +1"""
-
-
